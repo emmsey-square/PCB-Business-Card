d14289675ed796c0279c1c827ef405da7dc667a2
emmseysquare <emmseysquare.2@gmail.com>
2026-01-22 13:44:03 +0800

    fixed simulatneous button press bug, losing all lives results in game over

--- firmware/main.c
+++ firmware/main.c
@@ -1,14 +1,13 @@
 #include "ch32fun.h"
 #include "funconfig.h"
 
-
 #define SW1 PC3
 #define SW2 PC4
 
 uint8_t ledStrip[8] = {PC5, PC6, PD2, PD3, PD4, PD5, PD6, PC7};
 uint8_t lifeLeds[3] = {PC0, PC1, PC2};
 
-uint8_t lives = 4;
+uint8_t lives = 4;	
 uint32_t interval;
 uint32_t pos = 0;
 uint32_t prevLedTime, prevSwitchTime;
@@ -33,7 +32,12 @@ void clearLeds(void)
 uint32_t moveLeds(uint32_t* interval)
 {
 	static uint32_t pos = 0, dir  = 1;
-	if (lives > 0)
+
+	if (lives == 0)
+	{
+		clearLeds();
+	}
+	else
 	{
 		if ((millis() - prevLedTime) >= *interval)
 		{
@@ -52,15 +56,29 @@ uint32_t moveLeds(uint32_t* interval)
 			else if (pos <= 0)
 				dir = 1;
 		}
-	}
-	else 
-	{
-		clearLeds();
-	}
 
-	funDigitalWrite(ledStrip[pos], FUN_LOW);
+		funDigitalWrite(ledStrip[pos], FUN_LOW);
+	}
 
 	return pos;
+}
+
+void loseLife(void)
+{
+	Delay_Ms(500);
+	lives--;
+}
+
+void increaseSpeed(void)
+{
+	Delay_Ms(50);
+	interval -= Ticks_from_Ms(5);
+}
+
+void gameOver(void)
+{
+	clearLeds();
+	lives = 0;
 }
 
 void showLives(void)
@@ -86,11 +104,10 @@ void showLives(void)
 				funDigitalWrite(lifeLeds[i], FUN_LOW);
 			break;
 		default:
-			lives = 3;
+			gameOver();
 			break;
 	}
 }
-
 
 
 int main(void)
@@ -98,8 +115,6 @@ int main(void)
 	SystemInit();
 	funGpioInitAll();
 	
-	// Configure switch inputs
-
 	funPinMode(SW1, GPIO_Speed_In | GPIO_CNF_IN_FLOATING);
 	funPinMode(SW2, GPIO_Speed_In | GPIO_CNF_IN_FLOATING);
 	
@@ -107,15 +122,11 @@ int main(void)
 
 	prevLedTime = millis();
 
-	//Initialise GPIO's for each strip LED as push-pull output
-	
 	for (uint8_t i = 0; i < 8; i++)
 	{
 		funPinMode(ledStrip[i], GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
 	}
 	
-	// Initialise GPIO's for life LEDs as push-pull outputs
-
 	for (uint8_t i = 0; i < 3; i++)
 	{
 		funPinMode(lifeLeds[i], GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
@@ -130,20 +141,17 @@ int main(void)
 
 		pos = moveLeds(&interval);
 		
-		// Check if buttons are pressed when pos hits equals either end
-		
 		if ( (!sw1_state && (pos == 0)) || (!sw2_state && (pos == 7)) )
 		{
-			Delay_Ms(25);
-			interval -= Ticks_from_Ms(10);
+			increaseSpeed();
 		}
-
-		// Check if led at either end and respective buttons are pushed
-
-		if ( ((pos == 0) && (sw1_state == 1)) || ((pos == 7) && (sw2_state == 1)) )
+		else if ( ((pos == 0) && (sw1_state == 1)) || ((pos == 7) && (sw2_state == 1)) )	
 		{
-			Delay_Ms(500);
-			lives--;
+			loseLife();
+		}
+		else if (!sw1_state && !sw2_state)
+		{
+			loseLife();
 		}
 		
 		showLives();
